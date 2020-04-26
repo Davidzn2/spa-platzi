77d0fcb44a7445922a77b2da78a280a69398819a
david <david@labweb.mx>
2020-04-25 20:32:33 -0500

    initial cmmit

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import getData from "../utils/getData";
+
+const Home = async () => {
+  const characters = await getData();
+  console.log(characters);
+  const view = `
+    <div class="Characters">
+
+        ${characters.results
+          .map(
+            (character) => `
+            <article class="Character-item">
+                <a href="#/${character.id}/">
+                    <img src="${character.image}" alt="name">
+                    <h2>${character.name}</h2>
+                </a>
+            </article>
+            `
+          )
+          .join('')}
+       
+    </div>
+    `;
+  return view;
+};
+
+export default Home;
